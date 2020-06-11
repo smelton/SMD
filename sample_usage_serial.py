237e35e84eb1864053bfd27148601f04d4f22bec
smelton <smelton@users.noreply.github.com>
2020-06-11 17:45:53 -0400

    Add files via upload

---
+++ sample_usage_serial.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import make_blobs
+import numpy as np
+from SMD_serial import SMD
+
+def main():
+	N = 1000
+	#make 5 clusters in 10 dimensions
+	X_s,y = make_blobs(N,n_features = 10,centers = 5)
+	D_s = 10
+	#Add 100 noisy dimensions, so D_s = 10, and D_n = 100. D/D_s = (D_N + D_s)/D_s = 11
+	X = np.hstack([X_s,np.random.randn(N,100)])
+	D_n = 20
+	#Try SMD, pretend we don't know that there are 5 clusters, and guess 6
+	z = SMD(X,k_guess = 6,trials = 10)
+	print(f'Good dimensions have an average Z-score: {z[:D_s].mean():.2f} +/- {z[:D_s].std():.2f}\n')
+	print(f'Noisy dimensions have an average Z-score: {z[D_s:].mean():.2f} +/- {z[D_s:].std():.2f}')
+
+
+if __name__ == '__main__':
+	main()
