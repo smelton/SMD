43853d8e2be52b8a04193cd4c5c2bceff21d2442
smelton <smelton@users.noreply.github.com>
2020-06-12 11:33:05 -0400

    Add files via upload
    Speed/memory improvements

--- SMD.py
+++ SMD.py
@@ -32,7 +32,7 @@ from sklearn.svm import LinearSVC
 
 #The function will output an array of length(number of features), which is a z-score of the weight of each feature. Generally consider features with a value > 1
 
-def SMD(X,k_guess, trials = None,n_sub = None,prop_algo = 'agglo', class_algo = 'entropy', cluster_prior_min=None):
+def SMD(X,k_guess, trials = None,n_sub = None,prop_algo = 'agglo', class_algo = 'entropy', cluster_prior_min=None, z_score = True):
 	N = X.shape[0]
 	D = X.shape[1]
 
@@ -64,40 +64,45 @@ def SMD(X,k_guess, trials = None,n_sub = None,prop_algo = 'agglo', class_algo = 
 		print(class_algo, ' is not a supported classifier.')
 		return 0
 
-	cluster_prior = [3,int(2*k_guess)]
+	cluster_prior = [cluster_prior_min,int(2*k_guess)]
+	Xid = ray.put(X)
 
-	ray_jobs = [get_classifier_dims(X,n_sub, cluster_prior, cluster_algo) for _ in range(trials)]
-	ray_jobs = [i for k in ray_jobs for i in k]
-	counts = ray.get(ray_jobs)
+	ray_jobs = [get_classifier_dims.remote(X,n_sub, cluster_prior, cluster_algo) for _ in range(trials)]
+	ray_jobs = ray.get(ray_jobs)
+	counts = [i for k in ray_jobs for i in k]
 	gd,_ = np.histogram(counts, np.arange(X.shape[1]+1), density = True)
-	X_shuffled = shuffle_data(X)
-	ray_jobs = [get_classifier_dims(X_shuffled,n_sub, cluster_prior,cluster_algo) for _ in range(trials)]
-	ray_jobs = [i for k in ray_jobs for i in k]
-	counts = ray.get(ray_jobs)
-	g_shuffled,_ = np.histogram(counts, np.arange(X.shape[1]+1), density = True)
-	Z_d = (gd-g_shuffled.mean())/g_shuffled.std()
-	return Z_d
+	if z_score:
+		X_shuffled = shuffle_data(X)
+		ray_jobs = [get_classifier_dims.remote(X_shuffled,n_sub, cluster_prior,cluster_algo) for _ in range(trials)]
+		ray_jobs = ray.get(ray_jobs)
+		counts = [i for k in ray_jobs for i in k]
+		g_shuffled,_ = np.histogram(counts, np.arange(X.shape[1]+1), density = True)
+		Z_d = (gd-g_shuffled.mean())/g_shuffled.std()
+		return Z_d
+	else:
+		return gd
 
-
+@ray.remote
 def find_classifier_dims_entropy(X,cell_sample,k_sub, cluster_algo):
 	n_cells,n_genes = X.shape
 	cell_use = np.random.choice(np.arange(n_cells),cell_sample,replace = False)
 	k_sub_i = np.random.randint(k_sub[0],k_sub[1])
 	Xit = X[cell_use,:]
 	k_guess = cluster_algo(k_sub_i).fit_predict(Xit)
-	gnout = [one_tree.remote(Xit,k_guess,ikk1,ikk2) for ikk1 in np.unique(k_guess) for ikk2 in np.unique(k_guess) if ikk1<ikk2 ]
+	gnout = [one_tree(Xit,k_guess,ikk1,ikk2) for ikk1 in np.unique(k_guess) for ikk2 in np.unique(k_guess) if ikk1<ikk2 ]
 	return gnout
 
+@ray.remote
 def find_classifier_dims_maxmargin(X,cell_sample,k_sub, cluster_algo):
 	n_cells,n_genes = X.shape
 	cell_use = np.random.choice(np.arange(n_cells),cell_sample,replace = False)
 	k_sub_i = np.random.randint(k_sub[0],k_sub[1])
 	Xit = X[cell_use,:]
 	k_guess = cluster_algo(k_sub_i).fit_predict(Xit)
-	gnout = [one_plane.remote(Xit,k_guess,ikk1,ikk2) for ikk1 in np.unique(k_guess) for ikk2 in np.unique(k_guess) if ikk1<ikk2 ]
+	gnout = [one_plane(Xit,k_guess,ikk1,ikk2) for ikk1 in np.unique(k_guess) for ikk2 in np.unique(k_guess) if ikk1<ikk2 ]
 	return gnout
 
-@ray.remote
+
 def one_tree(Xit,k_guess,ik1,ik2):            
 	clf = DecisionTreeClassifier(criterion = 'entropy', max_depth = 1)
 	XTT = Xit[np.logical_or(k_guess==ik1,k_guess==ik2),:]
@@ -106,7 +111,6 @@ def one_tree(Xit,k_guess,ik1,ik2):
 	feature_pick = np.flatnonzero(clf.feature_importances_)[0]
 	return feature_pick
 
-@ray.remote
 def one_plane(Xit,k_guess,ik1,ik2):            
 	clf = LinearSVC(penalty = 'l1', dual = False, C=0.05)
 	XTT = Xit[np.logical_or(k_guess==ik1,k_guess==ik2),:]
--- sample_usage.py
+++ sample_usage.py
@@ -9,11 +9,11 @@ def main():
 	X_s,y = make_blobs(N,n_features = 10,centers = 5)
 	D_s = 10
 	#Add 100 noisy dimensions, so D_s = 10, and D_n = 100. D/D_s = (D_N + D_s)/D_s = 11
-	X = np.hstack([X_s,np.random.randn(N,100)])
 	D_n = 100
+	X = np.hstack([X_s,np.random.randn(N,D_n)])
 	#Try SMD, pretend we don't know that there are 5 clusters, and guess 6
 	ray.init()
-	z = SMD(X,k_guess = 6,trials = 100)
+	z = SMD(X,k_guess = 6,trials = 50)
 	print(f'Good dimensions have an average Z-score: {z[:D_s].mean():.2f} +/- {z[:D_s].std():.2f}\n')
 	print(f'Noisy dimensions have an average Z-score: {z[D_s:].mean():.2f} +/- {z[D_s:].std():.2f}')
 
